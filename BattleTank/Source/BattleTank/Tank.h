e0885288207c1a76fd1cbb8c1ba9b6c2a62fc3c0
Dustin Aguilar <dust.aguilar@gmail.com>
2017-06-07 00:32:46 -0700

    Added ability to move tank based on track movements, began implement fly-by-write controls for the tank

--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -6,6 +6,7 @@
 #include "Tank.generated.h"
 
 class UTankAimingComponent;
+class UTankMovementComponent;
 class UTankBarrel;
 class UTankTurret;
 class AProjectile;
@@ -32,6 +33,9 @@ public:
 
 protected:
 	UTankAimingComponent* TankAimingComponent = nullptr;
+
+	UPROPERTY(BlueprintReadOnly)
+	UTankMovementComponent* TankMovementComponent = nullptr;
 
 public:
 	// Called when the game starts or when spawned
---
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankTrack.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	// TODO clamp actual throttle value so player can't exceed expected speed limit
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	if (TankRoot)
+	{
+		TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Unable to cast track owner to UPrimitiveComponent"));
+	}
+}
